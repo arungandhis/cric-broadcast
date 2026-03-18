144191019300fbdb9e48300dd0fada62d90ef30d
arungandhis <144406712+arungandhis@users.noreply.github.com>
2026-03-18 12:35:44 -0400

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-
 console.log("Frontend JS loaded!");
 window.addEventListener("error", (e) => console.log("Global error:", e.message));
 window.addEventListener("unhandledrejection", (e) => console.log("Promise error:", e.reason));
+
+
+
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
